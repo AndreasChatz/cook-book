f8f6798db8a96693f6fc1862917665dde790d32f
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-18 18:39:11 +0300

    Create a Signup page and Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesComponent }      from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeStartComponent }  from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-
+import { RecipeEditComponent }   from './recipes/recipe-edit/recipe-edit.component';
+import { SignupComponent }       from './auth/signup/signup.component';
 const appRoutes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent, children: [
@@ -16,6 +16,7 @@ const appRoutes = [
     { path: ':id/edit', component: RecipeEditComponent },
   ]},
   { path: 'shopping-list', component: ShoppingListComponent},
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
