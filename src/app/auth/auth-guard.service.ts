f4660a72c28892a330e8a1e39285d7303f7b58b0
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-20 14:58:21 +0300

    Improvement: Redirect user to home page when access is unauthorized.

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -2,16 +2,23 @@ import { CanActivate,
          ActivatedRouteSnapshot,
          RouterStateSnapshot,}    from '@angular/router';
 import { Injectable, }            from '@angular/core';
+import { Router, }                from '@angular/router';
 
 import { AuthService }            from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private router:      Router,) {}
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): boolean {
-    return this.authService.isAuthenticated();
+    if (this.authService.isAuthenticated()){
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
   }
 }
