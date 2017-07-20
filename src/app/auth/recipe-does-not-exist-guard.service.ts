94dc6e58cb4f89c566225820646c8a40aac67b8a
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-20 21:16:08 +0300

    Fix: minor bug fix (ternary operator).

--- src/app/auth/recipe-does-not-exist-guard.service.ts
+++ src/app/auth/recipe-does-not-exist-guard.service.ts
@@ -7,7 +7,7 @@ import { Router, }                from '@angular/router';
 import { AuthService }            from './auth.service';
 import { RecipeService }          from '../recipes/recipe.service';
 
-// If the recipe does not exist, prevent the app to crash
+// If the recipe does not exist, prevent the app to crash.
 @Injectable()
 export class RecipeDoesNotExistGuard implements CanActivate {
 
@@ -26,6 +26,6 @@ export class RecipeDoesNotExistGuard implements CanActivate {
 
     // if recipe exist return true else redirect user to /recipes
     // and return false to prevent fetching the not existed data
-    return recipe ? true : this.router.navigate(['/recipes']), false;
+    return recipe ? true : (this.router.navigate(['/recipes']), false);
   }
 }
