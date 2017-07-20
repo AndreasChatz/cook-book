2cd1e02521c2ce4a280a494e860dea1c0483e933
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-20 10:25:05 +0300

    Signing Users up.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature: string = 'recipe';
 
   onNavigate(feature: string){
     this.loadedFeature = feature;
+  }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey:     "AIzaSyCUeXk_v5ocyzjnoOkwj7u3Tt0-8TxkVo8",
+      authDomain: "cook-book-ed64b.firebaseapp.com",
+    });
   }
 
 }
