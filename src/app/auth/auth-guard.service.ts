051a953bae9eed701be257cd9463f7167628c737
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-20 19:40:21 +0300

    Improvement: Redirect to /recipes when page not found. (wildcard)

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isAuthenticated()){
       return true;
     } else {
-      this.router.navigate(['/']);
+      this.router.navigate(['/recipes']);
       return false;
     }
   }
