d678e8664037ca94294126a12890903772826848
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-21 13:26:03 +0300

    Registering Routes in a Feature Module ( recipe.module.ts) and move the wildcard to recipes-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,47 @@
-import { BrowserModule }       from '@angular/platform-browser';
-import { NgModule }            from '@angular/core';
-import { FormsModule,
-        ReactiveFormsModule, } from '@angular/forms';
-import { HttpModule }          from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { FormsModule}    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }            from './app.component';
-import { HeaderComponent }         from './header/header.component';
-import { RecipesComponent }        from './recipes/recipes.component';
-import { RecipeListComponent }     from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent }   from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent }     from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent }   from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent }   from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective }       from './shared/dropdown.directive';
-import { ShoppingListService }     from './shopping-list/shopping-list.service';
-import { AppRoutingModule }        from './app-routing.module';
-import { RecipeStartComponent }    from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent }     from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeService }           from './recipes/recipe.service';
-import { DataStorageService }      from './shared/data-storage.service';
-import { SignupComponent }         from './auth/signup/signup.component';
-import { SigninComponent }         from './auth/signin/signin.component';
-import { AuthService }             from './auth/auth.service';
 import { AuthGuard }               from './auth/auth-guard.service';
+import { AuthService }             from './auth/auth.service';
+import { AppComponent }            from './app.component';
+import { RecipesModule }           from './recipes/recipes.module';
+import { RecipeService }           from './recipes/recipe.service';
+import { HeaderComponent }         from './header/header.component';
+import { SigninComponent }         from './auth/signin/signin.component';
+import { SignupComponent }         from './auth/signup/signup.component';
+import { AppRoutingModule }        from './app-routing.module';
+import { DropdownDirective }       from './shared/dropdown.directive';
+import { DataStorageService }      from './shared/data-storage.service';
+import { ShoppingListService }     from './shopping-list/shopping-list.service';
+import { ShoppingEditComponent }   from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent }   from './shopping-list/shopping-list.component';
 import { RecipeDoesNotExistGuard } from './auth/recipe-does-not-exist-guard.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
+    SigninComponent,
+    DropdownDirective,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
-    SignupComponent,
-    SigninComponent,
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpModule,
+    FormsModule,
+    BrowserModule,
+    RecipesModule,
     AppRoutingModule,
   ],
   providers: [
-    ShoppingListService,
+    AuthGuard,
+    AuthService,
     RecipeService,
     DataStorageService,
-    AuthService,
-    AuthGuard,
+    ShoppingListService,
     RecipeDoesNotExistGuard,
   ],
   bootstrap: [
