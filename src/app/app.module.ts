edba38459f11c58b21b9906c25188c34375db307
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-24 18:14:51 +0300

    Restructure Services to use the Child Injector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { NgModule }      from '@angular/core';
 import { HttpModule }    from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AuthGuard }               from './auth/auth-guard.service';
 import { AuthModule }              from './auth/auth.module';
 import { CoreModule }              from './core/core.module';
-import { AuthService }             from './auth/auth.service';
 import { AppComponent }            from './app.component';
 import { SharedModule }            from './shared/shared.module';
-import { RecipeService }           from './recipes/recipe.service';
 import { AppRoutingModule }        from './app-routing.module';
-import { DataStorageService }      from './shared/data-storage.service';
 import { ShoppingListModule }      from './shopping-list/shopping-list.module';
-import { ShoppingListService }     from './shopping-list/shopping-list.service';
-import { RecipeDoesNotExistGuard } from './auth/recipe-does-not-exist-guard.service';
 
 @NgModule({
   declarations: [
@@ -29,12 +23,7 @@ import { RecipeDoesNotExistGuard } from './auth/recipe-does-not-exist-guard.serv
     CoreModule,
   ],
   providers: [
-    AuthGuard,
-    AuthService,
-    RecipeService,
-    DataStorageService,
-    ShoppingListService,
-    RecipeDoesNotExistGuard,
+    // Metaferthikan sto core.module.ts
   ],
   bootstrap: [
     AppComponent,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 
-import { SharedModule }     from '../shared/shared.module';
-import { HomeComponent }    from './home/home.component';
-import { HeaderComponent }  from './header/header.component';
-import { AppRoutingModule } from '../app-routing.module';
+import { AuthGuard }               from '../auth/auth-guard.service';
+import { AuthService }             from '../auth/auth.service';
+import { SharedModule }            from '../shared/shared.module';
+import { HomeComponent }           from './home/home.component';
+import { RecipeService }           from '../recipes/recipe.service';
+import { HeaderComponent }         from './header/header.component';
+import { AppRoutingModule }        from '../app-routing.module';
+import { DataStorageService }      from '../shared/data-storage.service';
+import { ShoppingListService }     from '../shopping-list/shopping-list.service';
+import { RecipeDoesNotExistGuard } from '../auth/recipe-does-not-exist-guard.service';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +24,14 @@ import { AppRoutingModule } from '../app-routing.module';
   exports: [
     AppRoutingModule, // xriazetai???? :/
     HeaderComponent,
+  ],
+  providers: [
+    AuthGuard,
+    AuthService,
+    RecipeService,
+    DataStorageService,
+    ShoppingListService,
+    RecipeDoesNotExistGuard,
   ]
 })
 export class CoreModule {
