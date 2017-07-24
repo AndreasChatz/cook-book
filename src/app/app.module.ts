679acf3e9e2741a7f07d66611283832b53010f41
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-24 17:59:35 +0300

    Create a Basic Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AuthGuard }               from './auth/auth-guard.service';
 import { AuthModule }              from './auth/auth.module';
+import { CoreModule }              from './core/core.module';
 import { AuthService }             from './auth/auth.service';
 import { AppComponent }            from './app.component';
 import { SharedModule }            from './shared/shared.module';
 import { RecipeService }           from './recipes/recipe.service';
-import { HeaderComponent }         from './header/header.component';
 import { AppRoutingModule }        from './app-routing.module';
 import { DataStorageService }      from './shared/data-storage.service';
 import { ShoppingListModule }      from './shopping-list/shopping-list.module';
 import { ShoppingListService }     from './shopping-list/shopping-list.service';
 import { RecipeDoesNotExistGuard } from './auth/recipe-does-not-exist-guard.service';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
   ],
   imports: [
     AuthModule,
@@ -29,6 +26,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     ShoppingListModule,
+    CoreModule,
   ],
   providers: [
     AuthGuard,
