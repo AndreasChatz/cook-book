9be8d9c675a723d66789ba40fb0e4ef62a4eb04f
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-24 15:41:53 +0300

    Add Lazy Loading to the Recipes Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }                from '@angular/core';
 import { Routes, RouterModule }    from '@angular/router';
 
+import { HomeComponent }           from './home/home.component';
 import { ShoppingListComponent }   from './shopping-list/shopping-list.component';
 
-const appRoutes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+const appRoutes: Routes = [
+  // { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  { path: '', component: HomeComponent},
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
   { path: 'shopping-list', component: ShoppingListComponent},
   { path: '**', redirectTo: '/recipes'},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AuthModule }              from './auth/auth.module';
 import { AuthService }             from './auth/auth.service';
 import { AppComponent }            from './app.component';
 import { SharedModule }            from './shared/shared.module';
-import { RecipesModule }           from './recipes/recipes.module';
 import { RecipeService }           from './recipes/recipe.service';
 import { HeaderComponent }         from './header/header.component';
 import { AppRoutingModule }        from './app-routing.module';
@@ -15,17 +14,19 @@ import { DataStorageService }      from './shared/data-storage.service';
 import { ShoppingListModule }      from './shopping-list/shopping-list.module';
 import { ShoppingListService }     from './shopping-list/shopping-list.service';
 import { RecipeDoesNotExistGuard } from './auth/recipe-does-not-exist-guard.service';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     AuthModule,
     HttpModule,
     SharedModule,
     BrowserModule,
-    RecipesModule,
     AppRoutingModule,
     ShoppingListModule,
   ],
