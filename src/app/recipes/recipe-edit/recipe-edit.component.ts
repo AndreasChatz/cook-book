f2936e2ddc4067ed012e93b16c4bce335d120a27
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-07-25 14:44:21 +0300

    Fix: compilation errors when ng build --prod --aot

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -72,6 +72,10 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
